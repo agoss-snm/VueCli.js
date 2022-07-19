c5e091341f04641cc73ee78d9c207759aa82203b
Agostina Romero <agostinaromontamatt@gmail.com>
2022-07-18 22:41:38 -0300

    vuecli

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <AlumnoLista :Alum="Alum"/>
+  </div>
+</template>
+
+<script>
+import AlumnoLista from './components/AlumnoLista.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AlumnoLista,
+    
+  },
+  data(){
+    return{
+      Alum:[
+            {
+                'nombre': 'Jose',
+                'apellido': 'Piccioni',
+                'edad': '39',
+                'nota': '7'
+            },
+            {
+                'nombre': 'Fernando',
+                'apellido': 'Rivero',
+                'edad': '30',
+                'nota': '3'
+            },
+            {
+                'nombre': 'Gissella',
+                'apellido': 'Gastin',
+                'edad': '32',
+                'nota': '10'
+            },
+        ],
+    }
+  },
+  methods:{
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
